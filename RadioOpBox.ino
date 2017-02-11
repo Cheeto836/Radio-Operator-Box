ac5efe96e5a56385e9deef5ed1548ce4b0dc311a
Cheeto836 <matt.p.russell@aol.com>
2017-02-10 21:07:26 -0500

    updated file structure, added LabVIEW side tester

--- RadioOpBox.ino
+++ RadioOpBox.ino
@@ -1,110 +1,27 @@
-#include <Adafruit_MCP23017.h>
 #include <Wire.h>
-
-Adafruit_MCP23017 mcp;
-
-const int INT_PIN = 3;
-const int DEBOUNCE_MS = 50;
-const int TIMEOUT_MS = 100;
-
-const int numButtonsPortA = 2;
-const int portAButtons[] = {28, 23};
-const int numButtonsPortB = 2;
-const int portBButtons[] = {1, 4};
-
-uint16_t value = 0;
-bool valChanged = false;
-
-void setup()
-{
-  //configure serial for comm with dashboard
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
   Serial.begin(9600);
-
-  //configure the arduino interrupt pin
-  attachInterrupt(digitalPinToInterrupt(INT_PIN), handleUpdate, RISING);
-
-  //configure the MCP
-  mcp.begin(); //default address, all ADR pins wired LOW
-  mcp.setupInterrupts(false, false, HIGH); //interrupts not mirrored, floating,signaled with HIGH
-
-  //configure port a as input with interrupt condition rising
-  for (int i = 0; i < numButtonsPortA; i++)
-  {
-    mcp.pinMode(portAButtons[i], INPUT);
-    mcp.pullUp(portAButtons[i], HIGH);
-    mcp.setupInterruptPin(portAButtons[i], FALLING);
-  }
-  
-  //configure port b as input with interrupt condition rising
-  for (int i = 0; i < numButtonsPortB; i++)
-  {
-    mcp.pinMode(portBButtons[i], INPUT);
-    mcp.pullUp(portBButtons[i], HIGH);
-    mcp.setupInterruptPin(portBButtons[i], FALLING);
-  }
 }
-
-void loop()
-{
-  //only process if interrupt declares we changed values
-  if (valChanged)
-  {
-    //process command based on value
-    Serial.print("value: ");
-    Serial.println(value);
-    valChanged = false;
-  }
-  
-  Serial.print("value: ");
-  Serial.println(mcp.readGPIOAB(), BIN);
-}
-
-void handleUpdate()
-{
-  //assume the value is zero (no buttons pressed)
-  unsigned long refTime = millis();
-  bool stable = false;
-  bool timeout = false;
-  bool prevVal = false;
-  while (!stable && !timeout)
-  {
-    unsigned long curTime = millis();
-    unsigned long elapsed = curTime - refTime;
-    if (mcp.readGPIOAB() != 0)
-    {
-      //value true
-      if (prevVal)
-      {
-        //previous value same, no bounce]
-        if (elapsed >= DEBOUNCE_MS)
-        {
-          value = mcp.readGPIOAB();
-          valChanged = true;
-        }
-      }
-      else
-      {
-        //previous value different, bounce
-        refTime = millis();
-      }
-      //update previous value
-      prevVal = true;
-    }
-    else
-    {
-      //value false
-      if (!prevVal)
-      {
-        //previous value same, no bounce
-        timeout = (elapsed >= TIMEOUT_MS);
-      }
-      else
-      {
-        //previous value different, bounce
-        refTime = millis();
-      }
-      //update previous value
-      prevVal = false;
-    }
-  }
+uint8_t inputsA = 0;
+uint8_t inputsB = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(0x20);
+  Wire.write(0x13);
+  Wire.endTransmission();
+  Wire.requestFrom(0x20, 1);
+  inputsB = Wire.read();
+  Wire.beginTransmission(0x20);
+  Wire.write(0x12);
+  Wire.endTransmission();
+  Wire.requestFrom(0x20, 1);
+  inputsA = Wire.read();
+  uint16_t toTransmit = inputsA;
+  toTransmit <<= 8;
+  toTransmit |= inputsB;
+  Serial.print(toTransmit, BIN);
+  Serial.print("\n");
+  delay(200);
 }
